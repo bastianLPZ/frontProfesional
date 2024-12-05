b6c00a49831286c7e6d2be82f3c834a4f85b5ee6
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-12-05 03:15:45 -0300

    Chore: Mejora el layout de usuario

--- src/layouts/LayoutUsuario.jsx
+++ src/layouts/LayoutUsuario.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom'; // Este componente se usa para renderizar las rutas hijas
-
+import Sidebar from '../components/Usuario/Sidebar';
+import Navbar from '../components/Usuario/Navbar';
 const LayoutUsuario = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen w-screen overflow-x-hidden">
+      <div className="w-1/5"> {/* Ajusta el ancho del Sidebar aquí */}
+        <Sidebar />
+      </div>
       {/* Sidebar de navegación */}
      
 
       {/* Contenido principal */}
       <div className="flex-1 flex flex-col">
          {/* Encabezado que puede incluir el logo y el título de la página */}
-
-        <main className="flex-1 p-4 overflow-y-auto">
+        <Navbar />
+        <main className="flex-1 p-4 overflow-y-auto mt-16">
           <Outlet /> {/* Aquí se renderizarán las rutas hijas */}
         </main>
       </div>
